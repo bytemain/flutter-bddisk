305edc6b4926bba8352242014d38fa39e0acd1da
Artin <lengthmin@gmail.com>
2020-06-28 01:17:24 +0800

    feat: cleartextTrafficPermitted

--- android/app/src/main/kotlin/li/artin/bddisk/MainActivity.kt
+++ android/app/src/main/kotlin/li/artin/bddisk/MainActivity.kt
@@ -7,10 +7,11 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
+
     //设置状态栏沉浸式透明（修改flutter状态栏黑色半透明为全透明）
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
